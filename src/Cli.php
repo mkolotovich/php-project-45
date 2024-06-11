04c3010771d93606ec152a503301d3ebbbabbe8c
Mike Kolotovich <k2punk@gmail.com>
2024-06-11 14:37:32 +0300

    finish fourth step

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,8 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcomeUser() {
+function welcomeUser()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
