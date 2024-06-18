57aee728525b348bd4d744dabb27442f8f4e1205
Mike Kolotovich <k2punk@gmail.com>
2024-06-18 14:32:22 +0300

    start sixth step

--- src/Engine.php
+++ src/Engine.php
@@ -1,1 +1,30 @@
 <?php
+
+namespace BrainGames\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function playGame($question, $generateRoundData)
+{
+    $correctAnswersCount = 0;
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name', false, '? ');
+    line("Hello, %s!", $name);
+    $winAnswersCount = 3;
+    line($question);
+    while ($correctAnswersCount < $winAnswersCount) {
+        [$roundQuestion, $rAns] = $generateRoundData;
+        line('Question: %s', $roundQuestion);
+        $ans = prompt('Your answer ');
+        if ($ans == $rAns) {
+            line('Correct!');
+            $correctAnswersCount += 1;
+        } else {
+            line("'{$ans}' is wrong answer ;(. Correct answer was '{$rAns}'.");
+            line("Let's try again, {$name}!");
+            return;
+        }
+    }
+    line('Congratulations, %s!', $name);
+}
--- src/calc.php
+++ src/calc.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace BrainGames\Calc;
+
+require_once(__DIR__ . '/../src/Engine.php');
+use function BrainGames\Engine\playGame;
+
+function generateGameData()
+{
+    $operations = [['+', fn($a, $b) => $a + $b], ['-', fn($a, $b) =>$a - $b], ["*", fn($a, $b) => $a * $b]];
+    $firstNum = rand(1, 25);
+    $signsIndex = rand(0, count($operations) - 1);
+    $secondNum = rand(1, 25);
+    [$sign, $operation] = $operations[$signsIndex];
+    $question = "{$firstNum} {$sign} {$secondNum}";
+    $answer = (string) $operation($firstNum, $secondNum);
+    return [$question, $answer];
+}
+
+function startGame()
+{
+    $question = 'What is the result of the expression?';
+    playGame($question, generateGameData());
+}
